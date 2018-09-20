1e466dda148c7ef3f15438299221734a1a0748df
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-09-20 08:22:02 -0400

    Started

---
+++ src/NumberGuessing.java
@@ -0,0 +1,16 @@
+public class NumberGuessing {
+
+    public static void main(String[]args){
+
+        int pickedNumber;
+
+        pickedNumber = (int)(Math.random()*100);
+
+        System.out.println(pickedNumber);
+
+
+
+
+
+    }
+}
