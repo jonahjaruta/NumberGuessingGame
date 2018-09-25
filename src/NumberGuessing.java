eed4b7af7cf92202dffd303dc12e3472457343ef
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-09-25 07:58:03 -0400

    looped properly

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -8,58 +8,33 @@ public class NumberGuessing {
         int pickedNumber;
         Scanner keyboard;
         int answer1;
-        int answer2;
-        int answer3;
-        int answer4;
-        int answer5;
+        boolean win = false;
+
+
 
         //initialization
         keyboard = new Scanner(System.in);
         pickedNumber = (int) (Math.random() * 100);
 
+        System.out.println("welcome to my number guessing game!");
 
+        while(win == false ){
 
-        System.out.println("Let's play a game, try to guess my number 1-100.");
-        answer1 = keyboard.nextInt();
-
+            System.out.println("Try to guess the number 1-100.");
+            answer1 = keyboard.nextInt();
         if (pickedNumber == answer1) {
+            win = true;
             System.out.println("Correct! You win, well done");
-        } else if (pickedNumber < answer1) {
-            System.out.println("Too high, guess again.");
-        } else if (pickedNumber > answer1) {
-            System.out.println("Too low, try again.");
+        }
+        else if (pickedNumber < answer1) {
+            System.out.println("Too high.");
+        }
+        else if (pickedNumber > answer1) {
+            System.out.println("Too low.");
         }
 
-        answer2 = keyboard.nextInt();
 
-        if (pickedNumber == answer2) {
-            System.out.println("Correct! You win, well done");
-        } else if (pickedNumber < answer2) {
-            System.out.println("Too high, guess again.");
-        } else if (pickedNumber > answer2) {
-            System.out.println("Too low, try again.");
-        }
-
-        answer3 = keyboard.nextInt();
-
-        if (pickedNumber == answer3) {
-            System.out.println("Correct! You win, well done");
-        } else if (pickedNumber < answer3) {
-            System.out.println("Too high, guess again.");
-        } else if (pickedNumber > answer3) {
-            System.out.println("Too low, try again.");
-        }
-
-        answer4 = keyboard.nextInt();
-
-        if (pickedNumber == answer4) {
-            System.out.println("Correct! You win, well done");
-        } else if (pickedNumber < answer4) {
-            System.out.println("Too high, guess again.");
-        } else if (pickedNumber > answer4) {
-            System.out.println("Too low, try again.");
-        }
 
 
     }
-}
+}}
