16b0d13f575f6d0e62acdbe7b23a3179587b604a
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-10-04 07:48:58 -0400

    Updated as Required

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -5,36 +5,46 @@ public class NumberGuessing {
     public static void main(String[] args) {
 
         //declare
+        boolean runAgain = false;
         int pickedNumber;
         Scanner keyboard;
+
+        while (runAgain == false) {
+
         int answer1;
         boolean win = false;
+        int tries = 0;
 
 
 
         //initialization
         keyboard = new Scanner(System.in);
+
+
+
         pickedNumber = (int) (Math.random() * 100);
 
         System.out.println("welcome to my number guessing game!");
 
-        while(win == false ){
+
+        while (win == false) {
 
             System.out.println("Try to guess the number 1-100.");
             answer1 = keyboard.nextInt();
-        if (pickedNumber == answer1) {
-            win = true;
-            System.out.println("Correct! You win, well done");
+            tries++;
+
+            if (pickedNumber == answer1) {
+                win = true;
+                System.out.println("Correct! You win, well done");
+            } else if (pickedNumber < answer1) {
+                System.out.println("Too high.");
+            } else if (pickedNumber > answer1) {
+                System.out.println("Too low.");
+            }
+
+
         }
-        else if (pickedNumber < answer1) {
-            System.out.println("Too high.");
+        System.out.println("It took you " + tries + " tries.");
         }
-        else if (pickedNumber > answer1) {
-            System.out.println("Too low.");
-        }
-
-
-
-
     }
-}}
+}
